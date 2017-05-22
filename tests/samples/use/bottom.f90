22362ff076791473076e3c6d3e3c5f8598d862d1
Christian Hovy <hovy@informatik.uni-hamburg.de>
2017-05-22 11:31:44 +0200

    Started with test for UseTraveler

---
+++ tests/samples/use/bottom.f90
@@ -0,0 +1,9 @@
+MODULE bottom
+
+  SUBROUTINE butt()
+
+    WRITE (*,*) 'Deep down'
+
+  END SUBROUTINE medium
+
+END MODULE bottom
---
+++ tests/samples/use/top.f90
@@ -0,0 +1,11 @@
+MODULE top
+
+  USE middle, ONLY :: medium
+
+  SUBROUTINE tiptop()
+
+    CALL medium()
+
+  END SUBROUTINE tiptop
+
+END MODULE top
