5ce71c156d19525089cecc09df987d3223650366
Christian Hovy <hovy@informatik.uni-hamburg.de>
2017-12-01 11:26:42 +0100

    [tests] Change PROGRAM to MODULE in assignment test

--- tests/samples/assignment/assignment.f90
+++ tests/samples/assignment/assignment.f90
@@ -1,4 +1,4 @@
-PROGRAM assignment
+MODULE assignment
 
   IMPLICIT NONE
 
@@ -7,22 +7,34 @@ PROGRAM assignment
     INTEGER :: second
     INTEGER :: third
   END TYPE ttest
+!
+!  INTERFACE operator (+)
+!    MODULE PROCEDURE ttadd
+!  END INTERFACE
 
   TYPE :: membertest
     TYPE(ttest), POINTER :: ptr
   END TYPE membertest
 
-  TYPE(ttest) :: test
-
-  test%first = 23
-  test%second = 42
-  test%third = 109
-
-  CALL testDirect(test)
-  CALL testIndirect(test)
-  CALL testMember(test)
+!  TYPE(ttest) :: test
+!
+!  test%first = 23
+!  test%second = 42
+!  test%third = 109
+!
+!  CALL testDirect(test)
+!  CALL testIndirect(test)
+!  CALL testMember(test)
 
 CONTAINS
+
+!  FUNCTION ttadd(tt1, tt2) result (tt3)
+!   TYPE(ttest), intent(in) :: tt1, tt2
+!   TYPE(ttest) :: tt3
+!
+!   tt3%first = tt1%first + tt2%first ! Note the use of array operations
+!
+!  END FUNCTION ttadd
 
   SUBROUTINE testDirect(inarg)
 
@@ -67,4 +79,4 @@ CONTAINS
 
   END SUBROUTINE testMember
 
-END PROGRAM assignment
+END MODULE assignment
