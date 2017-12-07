633d4f23374a94c9953329a65a30833f0a616240
Christian <christian@hovy.de>
2017-12-07 23:40:40 +0100

    Moved loadConfiguration() to new module

--- FortranCallGraph.py
+++ FortranCallGraph.py
@@ -7,7 +7,6 @@
 import sys;
 import argparse;
 import re
-import os
 
 from source import SubroutineFullName, SourceFiles
 from tree import TreeLikeCallGraphPrinter;
@@ -20,9 +19,9 @@ from dumper import SourceLineDumper, SourceStatementDumper
 from linenumbers import DeclarationLineNumberFinder, EndStatementLineNumberFinder, FirstDocumentationLineFinder, LastSpecificationLineFinder, AllLineFinder,\
     LastUseLineFinder, ContainsLineFinder
 from useprinter import UsedModuleNamePrinter, UsedFileNamePrinter
-from utils import assertType
 from assembler import FromAssemblerCallGraphBuilder
 from treecache import CachedAssemblerCallGraphBuilder
+from configurator import loadConfiguration
 
 GRAPH_PRINTERS = {'tree': 'in a tree-like form',
                   'dot': 'in DOT format for Graphviz',
@@ -102,57 +101,6 @@ def parseArguments():
     argParser.add_argument('module');
     argParser.add_argument('subroutine', nargs='?', default=None);
     return argParser.parse_args();
-
-def loadConfiguration(configFile):
-    assertType(configFile, 'configFile', str, True)
-    
-    if configFile is None:
-        configFile = 'config_fortrancallgraph.py'
-    originalConfigFile = configFile
-    if not os.path.isfile(configFile):
-        configFile = os.path.dirname(os.path.realpath(__file__)) + '/' + originalConfigFile
-    if not os.path.isfile(configFile):
-        configFile = os.path.dirname(os.path.realpath(__file__)) + '/config/' + originalConfigFile
-    if not os.path.isfile(configFile):
-        print >> sys.stderr, 'Config file not found: ' + originalConfigFile
-        return None
-        
-    config = {}
-    execfile(configFile, globals(), config)
-    
-    configError = False
-    if not config['SOURCE_DIR']:
-        print >> sys.stderr, 'Missing config variable: SOURCE_DIR'
-        configError = True
-    elif isinstance(config['SOURCE_DIR'], str):
-        config['SOURCE_DIR'] = [config['SOURCE_DIR']]
-
-    if not config['ASSEMBLER_DIR']:
-        print >> sys.stderr, 'Missing config variable: ASSEMBLER_DIR'
-        configError = True
-    elif isinstance(config['ASSEMBLER_DIR'], str):
-        config['ASSEMBLER_DIR'] = [config['ASSEMBLER_DIR']]
-        
-    if not config['SPECIAL_MODULE_FILES']:
-        config['SPECIAL_MODULE_FILES'] = {}
-
-    if not config['CACHE_DIR']:
-        config['CACHE_DIR'] = None
-
-    if not config['EXCLUDE_MODULES']:
-        config['EXCLUDE_MODULES'] = []
-
-    if not config['IGNORE_GLOBALS_FROM_MODULES']:
-        config['IGNORE_GLOBALS_FROM_MODULES'] = []
-
-    if not config['IGNORE_DERIVED_TYPES']:
-        config['IGNORE_DERIVED_TYPES'] = []
-    
-    if configError:
-        return None
-    
-    return config
-
 
 def main():
     args = parseArguments()
