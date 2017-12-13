69f322bd0f6f02ecfe38c9af5c84fb6f741cd553
Christian Hovy <hovy@informatik.uni-hamburg.de>
2017-12-13 10:48:14 +0100

    Updated sample config file

--- config_fortrancallgraph.py
+++ config_fortrancallgraph.py
@@ -1,23 +1,15 @@
-from source import SourceFiles
-from treecache import CachedAssemblerCallGraphBuilder
-from assembler import FromAssemblerCallGraphBuilder
 import os
-
-
-### EDIT HERE ###
 
 FCG_DIR = os.path.dirname(os.path.realpath(__file__))
 CACHE_DIR = FCG_DIR + '/cache'
 
-ASSEMBLER_DIR = ''
-SOURCE_DIR = ''
+ASSEMBLER_DIRS = [] 
+
+SOURCE_DIRS = [] 
+SOURCE_FILES_PREPROCESSED = False 
 
 SPECIAL_MODULE_FILES = {}
 EXCLUDE_MODULES = []
-IGNORE_GLOBALS_FROM_MODULES = [] 
+
+IGNORE_GLOBALS_FROM_MODULES = EXCLUDE_MODULES + [] 
 IGNORE_DERIVED_TYPES = []
-
-#################
-
-GRAPH_BUILDER = CachedAssemblerCallGraphBuilder(CACHE_DIR, FromAssemblerCallGraphBuilder(ASSEMBLER_DIR, SPECIAL_MODULE_FILES))
-SOURCE_FILES = SourceFiles(SOURCE_DIR, SPECIAL_MODULE_FILES)
