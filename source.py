cc3fc344e548ae2524cc9244dcbf555b61068221
Christian Hovy <hovy@informatik.uni-hamburg.de>
2018-02-05 22:41:17 +0100

    Removed unused imports from source.py

--- source.py
+++ source.py
@@ -5,9 +5,6 @@ import re
 import sys
 from assertions import assertType, assertTypeAll
 from operator import attrgetter
-from os.path import stat
-from gtk.keysyms import at
-from mercurial.templater import sub
 
 IDENTIFIER_REG_EX = re.compile('^[a-z0-9_]{1,63}$', re.IGNORECASE)
 
