b015690021de5f88fce7aecb9c55aa2e42946b21
Christian Hovy <hovy@informatik.uni-hamburg.de>
2019-01-24 10:30:45 +0100

    Added TestSuite module containing all unit tests.

---
+++ tests/TestSuite.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+
+import unittest
+
+# import your test modules
+import TestInner
+import TestMod
+import TestUse
+import TestFunctions
+import TestNested
+import TestTypes
+import TestAssignment
+import TestBrackets
+import TestRecursion
+import TestVariableReference
+import TestTypeProcedure
+import TestOutVars
+import TestPreprocessed
+
+# initialize the test suite
+loader = unittest.TestLoader()
+suite  = unittest.TestSuite()
+
+# add tests to the test suite
+suite.addTests(loader.loadTestsFromModule(TestInner))
+suite.addTests(loader.loadTestsFromModule(TestMod))
+suite.addTests(loader.loadTestsFromModule(TestUse))
+suite.addTests(loader.loadTestsFromModule(TestFunctions))
+suite.addTests(loader.loadTestsFromModule(TestNested))
+suite.addTests(loader.loadTestsFromModule(TestTypes))
+suite.addTests(loader.loadTestsFromModule(TestAssignment))
+suite.addTests(loader.loadTestsFromModule(TestBrackets))
+suite.addTests(loader.loadTestsFromModule(TestRecursion))
+suite.addTests(loader.loadTestsFromModule(TestVariableReference))
+suite.addTests(loader.loadTestsFromModule(TestTypeProcedure))
+suite.addTests(loader.loadTestsFromModule(TestOutVars))
+suite.addTests(loader.loadTestsFromModule(TestPreprocessed))
+
+# initialize a runner, pass it your suite and run it
+runner = unittest.TextTestRunner(verbosity=3)
+result = runner.run(suite)
