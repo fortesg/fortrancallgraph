bbe753ee026433fe83ef43ccf693922ef74badcf
Christian Hovy <hovy@informatik.uni-hamburg.de>
2019-01-25 11:37:58 +0100

    Renamed print module to printout

--- printout.py
+++ printout.py
@@ -8,10 +8,10 @@ def printLines(lines):
     print('\n'.join(lines))
     
 def printError(line, location = '', exitCode = 0):
-    msg = 'ERROR'
+    msg = '*** ERROR'
     if location:
         msg += ' [' + location + ']'
-    msg += ': ' + line
+    msg += ': ' + line + ' ***'
     print(msg, file=sys.stderr)
     if exit != 0:
         sys.exit(exitCode)
